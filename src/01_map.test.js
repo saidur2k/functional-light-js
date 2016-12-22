8423619682ed43876a35dbdfb39b3fdce4b5cb38
Saidur <saidur@rudias.com>
2016-12-22 23:39:23 +1100

    test:

--- src/01_map.test.js
+++ src/01_map.test.js
@@ -6,13 +6,10 @@ describe('map', () => {
   })
 
   it('transforms', () => {
-    expect([1,2,3,4,5].map(doubleIt)).to.deep.equal([2,4,6,8,10])
+    expect([1, 2, 3, 4, 5].map(doubleIt)).to.deep.equal([2, 4, 6, 8, 10])
   })
 })
 
-function doubleIt(v) {
+function doubleIt (v) {
   return v * 2
 }
-
-Array.map() method can be used to create immutable list transformations.
-  The new list of transformed values has a one-to-one “mapping” with the original list.
